a36186ec2cca37cac49684d9c2e198f81e7e1f2e
ausias <ausias@hINFO6-4.xiam.cat>
2023-01-17 12:00:41 +0100

    Commit 2 dia 17Gen

--- ProjecteLoteria/src/projecteloteria/ProjecteLoteria.java
+++ ProjecteLoteria/src/projecteloteria/ProjecteLoteria.java
@@ -13,17 +13,22 @@ public class ProjecteLoteria {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int numLoteria = validarEntero("Introduce tu número de la loteria: ");
-        int resultat = LlargadaNum(numLoteria);
+        //int numLoteria = validarEntero("Introduce tu número de la loteria: ");
+        int premi1 = premiGordo();
+        int premi2 = segonPremi();
+        int premi3 = tercerPremi();
+        System.out.println(premi1);
+        System.out.println(premi2);
+        System.out.println(premi3);
     }
-    public static int LlargadaNum (int num){
+    /*public static int LlargadaNum (int num){
         num = scan.nextInt();
         num = String.valueOf(num).length();
         while (num != 5){
            num = scan.nextInt();
         }
         return num;
-    }
+    }*/
     static int validarEntero(String mensaje){
         Scanner scan = new Scanner(System.in);
         int valor;
@@ -36,4 +41,19 @@ public class ProjecteLoteria {
         return valor;
     }
     
+    static int premiGordo (){
+        Random rnd=new Random();
+        int aleatori=rnd.nextInt(00001 + 99999);
+        return aleatori;
+    }
+    static int segonPremi (){
+        Random rnd=new Random();
+        int aleatori=rnd.nextInt(00001 + 99999);
+        return aleatori;
+    }
+    static int tercerPremi (){
+        Random rnd=new Random();
+        int aleatori=rnd.nextInt(00001 + 99999);
+        return aleatori;
+    }
 }
